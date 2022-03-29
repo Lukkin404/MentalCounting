7ed677a8eec52b37f0714d80de1d022a34e76be3
Guillaume <73890954+Lukkin404@users.noreply.github.com>
2022-03-29 15:28:08 +0100

    avancement 1

--- app/src/main/java/com/example/mentalcounting/MainActivity.java
+++ app/src/main/java/com/example/mentalcounting/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.mentalcounting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Init button
+        Button playButton = findViewById(R.id.new_game_button);
+        Button scoreButton = findViewById(R.id.score_button);
+
+        //Init button listener
+        playButton.setOnClickListener(view -> openGame());
+        scoreButton.setOnClickListener(view-> openScore());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_main,menu);
+        return true;
+    }
+
+
+    private void openScore() {
+        Intent openScore = new Intent(this, ScoreActivity.class);
+        startActivity(openScore);
+    }
+
+
+    private void openGame() {
+        Intent open_Game = new Intent(this, GameActivity.class);
+        startActivity(open_Game);
     }
 }
